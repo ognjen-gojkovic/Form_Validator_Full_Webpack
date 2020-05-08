7322a68bc0d360884badc3d62f047c2c426f08f0
Ognjen Gojković <ognjen1804@gmail.com>
2020-05-09 00:46:01 +0200

    initial setup

---
+++ webpack.common.js
@@ -0,0 +1,23 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+  entry: "./src/assets/js/index.js",
+  module: {
+    rules: [
+      {
+        test: /\.(js)$/,
+        use: [
+          {
+            loader: "babel-loader",
+            options: {
+              presets: ["@babel/preset-env"],
+            },
+          },
+        ],
+      },
+    ],
+  },
+};
---
+++ webpack.dev.js
@@ -0,0 +1,71 @@
+const path = require("path");
+const merge = require("webpack-merge");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const common = require("./webpack.common");
+
+module.exports = merge(common, {
+  output: {
+    filename: "./assets/js/[name].bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    publicPath: "",
+  },
+  devServer: {
+    contentBase: path.resolve(__dirname, "dist"),
+    port: 5000,
+    compress: true,
+    historyApiFallback: true,
+    open: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(css)$/,
+        use: [
+          { loader: MiniCssExtractPlugin.loader },
+          {
+            loader: "css-loader",
+            options: {
+              modules: false,
+              //   modules: {
+              //     localIdentName: "[local]_[hash:base64:5]",
+              //   },
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(png|jpg|gif|svg)$/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              name: "[name].[ext]",
+              outputPath: "./assets/img",
+            },
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "./assets/css/[name].css",
+    }),
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "src", "index.html"),
+      inject: true,
+      minify: {
+        removeComments: false,
+        collapseWhitespace: false,
+      },
+    }),
+    new CopyWebpackPlugin([
+      {
+        from: "./src/img",
+        to: "./assets/img/[name].[ext]",
+      },
+    ]),
+  ],
+});
